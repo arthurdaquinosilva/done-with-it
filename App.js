a0df08f650161b840c9b32d30d0363c4535e77b5
arthurdaquinosilva <arthurdaquinosilva@gmail.com>
2025-12-29 09:28:54 -0300

    (fundamental-concepts): image

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
-import { StyleSheet, Text } from "react-native";
+import { Image, StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  const handlePress = () => console.log("Text pressed");
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Text numberOfLines={2}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et voluptate
-          quisquam blanditiis quidem dicta praesentium, beatae assumenda sed
-          temporibus vel dignissimos excepturi ipsam optio, unde sequi placeat.
-          Eos iste quibusdam ut ipsam necessitatibus expedita. Sit rem minima
-          molestiae minus, dignissimos obcaecati autem beatae omnis
-          necessitatibus similique quibusdam doloribus voluptatibus facilis
-          aliquam sunt quae non quam alias aspernatur animi est! Temporibus
-          ipsum dolores modi magnam aliquid totam iste, accusantium accusamus,
-          nihil tenetur quam illo aspernatur esse, nostrum rem veritatis neque
-          velit optio expedita recusandae! Cupiditate voluptatem, incidunt iure,
-          iste commodi modi quasi, architecto deserunt reiciendis ratione hic.
-          Eaque a aliquam corporis?
-        </Text>
-
-        <Text onPress={handlePress}>Press this text</Text>
+        <Text>Hello React Native</Text>
+        <Image
+          source={require("./assets/icon.png")}
+          style={{ width: 80, height: 80 }}
+        />
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+          resizeMode="cover"
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -32,5 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
